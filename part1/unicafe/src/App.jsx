a1000a371dd529a07ff98f9934a9ecc4a08627ab
Mykhailo Doroshenko <doroshenko.misha73@gmail.com>
2025-11-14 12:40:08 +0200

    Unicafe Step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,15 +6,38 @@ const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
 const FeedbackCounter = ({counter, text }) => <p>{text} {counter}</p>
 
+const AverageScore = ({good, neutral, bad}) => {
+  const calculateAverage = (good, neutral, bad) => (good - bad) / (good + neutral + bad)
+  return <p>average {calculateAverage(good=good, neutral=neutral, bad=bad)}</p>
+}
+
+const PositiveScore = ({good, all}) => {
+  const calculatePercentage = (good, all) => good / all * 100
+  return <p>positive {calculatePercentage(good=good, all=all)} %</p>
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
-  const handleClickGood = () => setGood(good + 1)
-  const handleClickNeutral = () => setNeutral(neutral + 1)
-  const handleClickBad = () => setBad(bad + 1)
+  const handleClickGood = () => {
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    setAll(updatedGood + neutral + bad)
+  }
+  const handleClickNeutral = () => {
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    setAll(good + updatedNeutral + bad)
+  }
+  const handleClickBad = () => {
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    setAll(good + neutral + updatedBad)
+  }
 
   return (
     <div>
@@ -27,6 +50,9 @@ const App = () => {
       <FeedbackCounter counter={good} text='good'/>
       <FeedbackCounter counter={neutral} text='neutral'/>
       <FeedbackCounter counter={bad} text='bad'/>
+      <FeedbackCounter counter={all} text='all'/>
+      <AverageScore good={good} neutral={neutral} bad={bad}/>
+      <PositiveScore good={good} all={all}/>
       
     </div>
   )
